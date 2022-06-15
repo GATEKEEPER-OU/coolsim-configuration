acd7b7de69b66926374f8a9263b0f4b3ef85248f
Alessio <dottorpollo@gmail.com>
2022-06-15 17:49:17 +0100

    gitignore and refactory dependencies

--- User/roles.js
+++ User/roles.js
@@ -1,4 +1,4 @@
-import Utils from '../../coolsim-utilities/index.js';
+import Utils from '../../utilities/index.js';
 
 
 // agent's roles
--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 import USER from "./User/index.js";
 import DEVICE from "./Device/index.js";
 import LOCATION from "./Location/index.js";
-
+// import store
 
 // todo create entry in the master db with list of simulations
 // todo copy templates in the details DB of the simulation
+
+// class config
+// constructor > simulation id > json
+// set > update and save new json
+// get > return config 
+// return config 
+
+export default class Config {
+    
+    constructor (simulationId){
+        // check or create new simulation
+    }
+    // set setAge(age){
+    // }
+    // get list(){
+    // };
+    static get simulations(){
+        // return simulations in store
+    }
+}
 
 
 
